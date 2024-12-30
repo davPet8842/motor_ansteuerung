238167065317910ed642c093f02108ced2a27ac5
davPet8842 <124050808+davPet8842@users.noreply.github.com>
2024-12-30 20:49:22 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,77 @@
+function Motor_an_rechts () {
+    images.arrowImage(ArrowNames.West).showImage(0)
+    speed = 50
+    MiniCar.motor(Motorlist.M1, Direction1.Forward, speed)
+    MiniCar.motor(Motorlist.M2, Direction1.Backward, speed)
+    basic.pause(600)
+    Motor_aus()
+}
+function LED_An (rot: number, grün: number, blau: number) {
+    MiniCar.PWM_LED_R(pwm_led_r.pem_red_l, 255 - rot)
+    MiniCar.PWM_LED_R(pwm_led_r.pwm_green_l, 255 - grün)
+    MiniCar.PWM_LED_R(pwm_led_r.pwm_blue_l, 255 - blau)
+    MiniCar.PWM_LED_L(pwm_led_l.pwm_red_r, 255 - rot)
+    MiniCar.PWM_LED_L(pwm_led_l.pwm_green_r, 255 - grün)
+    MiniCar.PWM_LED_L(pwm_led_l.pwm_blue_r, 255 - blau)
+}
+function LED_Aus () {
+    MiniCar.PWM_LED_R(pwm_led_r.pem_red_l, 255)
+    MiniCar.PWM_LED_R(pwm_led_r.pwm_green_l, 255)
+    MiniCar.PWM_LED_R(pwm_led_r.pwm_blue_l, 255)
+    MiniCar.PWM_LED_L(pwm_led_l.pwm_red_r, 255)
+    MiniCar.PWM_LED_L(pwm_led_l.pwm_green_r, 255)
+    MiniCar.PWM_LED_L(pwm_led_l.pwm_blue_r, 255)
+}
+input.onSound(DetectedSound.Loud, function () {
+    for (let index = 0; index < 4; index++) {
+        LED_An(0, 0, 255)
+        Motor_an_vor(255)
+        basic.pause(2000)
+        Motor_aus()
+        LED_Aus()
+        basic.pause(500)
+        LED_An(0, 255, 0)
+        Motor_an_rechts()
+        LED_Aus()
+    }
+})
+function Motor_an_zurück (geschwindigkeit: number) {
+    images.arrowImage(ArrowNames.South).showImage(0)
+    MiniCar.motor(Motorlist.M1, Direction1.Backward, geschwindigkeit)
+    MiniCar.motor(Motorlist.M2, Direction1.Backward, geschwindigkeit)
+}
+function Motor_an_vor (geschwindigkeit: number) {
+    images.arrowImage(ArrowNames.North).showImage(0)
+    MiniCar.motor(Motorlist.M1, Direction1.Forward, geschwindigkeit)
+    MiniCar.motor(Motorlist.M2, Direction1.Forward, geschwindigkeit)
+}
+function Motor_aus () {
+    speed = 0
+    MiniCar.motor(Motorlist.M1, Direction1.Forward, speed)
+    MiniCar.motor(Motorlist.M2, Direction1.Forward, speed)
+    Display_Löschen()
+}
+function Display_Löschen () {
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).showImage(0)
+}
+function Motor_an_links () {
+    images.arrowImage(ArrowNames.East).showImage(0)
+    speed = 50
+    MiniCar.motor(Motorlist.M1, Direction1.Backward, speed)
+    MiniCar.motor(Motorlist.M2, Direction1.Forward, speed)
+    basic.pause(600)
+    Motor_aus()
+    Display_Löschen()
+}
+let speed = 0
+Motor_aus()
+LED_Aus()
+basic.forever(function () {
+	
+})
