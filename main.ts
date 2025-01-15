88744aacc3ffa38d038b1d61ae39d8ff2e3abd5b
davPet8842 <124050808+davPet8842@users.noreply.github.com>
2025-01-15 20:37:38 +0100

    Display_rausgenommen

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
-let speed = 0
+function Motor_an_rechts (speed: number) {
+    //images.arrowImage(ArrowNames.East).showImage(0)
+    MiniCar.motor(Motorlist.M1, Direction1.Forward, speed)
+    MiniCar.motor(Motorlist.M2, Direction1.Backward, speed)
+}
 function LED_An (rot: number, grün: number, blau: number) {
     MiniCar.PWM_LED_R(pwm_led_r.pem_red_l, 255 - rot)
     MiniCar.PWM_LED_R(pwm_led_r.pwm_green_l, 255 - grün)
@@ -21,46 +25,35 @@ function Motor_an_zurück (geschwindigkeit: number) {
     MiniCar.motor(Motorlist.M2, Direction1.Backward, geschwindigkeit)
 }
 function Motor_an_vor (geschwindigkeit: number) {
-    images.arrowImage(ArrowNames.North).showImage(0)
+    //images.arrowImage(ArrowNames.North).showImage(0)
     MiniCar.motor(Motorlist.M1, Direction1.Forward, geschwindigkeit)
     MiniCar.motor(Motorlist.M2, Direction1.Forward, geschwindigkeit)
 }
 function Motor_aus () {
-    speed = 0
-    MiniCar.motor(Motorlist.M1, Direction1.Forward, speed)
-    MiniCar.motor(Motorlist.M2, Direction1.Forward, speed)
-    Display_Löschen()
+    MiniCar.motor(Motorlist.M1, Direction1.Forward, 0)
+    MiniCar.motor(Motorlist.M2, Direction1.Forward, 0)
+    //Display_Löschen()
 }
 function Motor_rechts_drehen () {
     images.arrowImage(ArrowNames.West).showImage(0)
-    speed = 50
-    MiniCar.motor(Motorlist.M1, Direction1.Forward, speed)
-    MiniCar.motor(Motorlist.M2, Direction1.Backward, speed)
+    MiniCar.motor(Motorlist.M1, Direction1.Forward, 50)
+    MiniCar.motor(Motorlist.M2, Direction1.Backward, 50)
     basic.pause(600)
     Motor_aus()
 }
 function Motor_links_drehen () {
     images.arrowImage(ArrowNames.East).showImage(0)
-    speed = 50
-    MiniCar.motor(Motorlist.M1, Direction1.Backward, speed)
-    MiniCar.motor(Motorlist.M2, Direction1.Forward, speed)
+    MiniCar.motor(Motorlist.M1, Direction1.Backward, 50)
+    MiniCar.motor(Motorlist.M2, Direction1.Forward, 50)
     basic.pause(600)
     Motor_aus()
     Display_Löschen()
 }
 function Display_Löschen () {
-    images.createImage(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `).showImage(0)
+    basic.clearScreen()
 }
-
-function Motor_an_links(speed: number)
-{
-    images.arrowImage(ArrowNames.East).showImage(0)
+function Motor_an_links (speed: number) {
+    //images.arrowImage(ArrowNames.East).showImage(0)
     MiniCar.motor(Motorlist.M1, Direction1.Backward, speed)
     MiniCar.motor(Motorlist.M2, Direction1.Forward, speed)
 }
